7a8fca29ea25ba79da75b68c002755dc8cf597d7
dshreddy03 <112101014@smail.iitpkd.ac.in>
2023-10-10 03:20:35 +0530

    update 4

--- js/intro.js
+++ js/intro.js
@@ -4,10 +4,10 @@ window.addEventListener('load', function() {
     const titleText = document.querySelector('.title-text');
 
     if (window.innerWidth <= 1200) {
-      console.log("phone");
+      // console.log("phone");
       titleText.style.marginTop = '40%';
     } else {
-      console.log("laptop");
+      // console.log("laptop");
       titleText.style.marginTop = '10%';
     }
 });  
